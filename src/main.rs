777ef6ff9f2a68b62530ba64df6c9fe584027207
discite-mac <4948877+discite@users.noreply.github.com>
2021-03-09 02:29:13 -0500

    feat: making echo on latinify command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env;
+use std::{env};
+use serde::{Deserialize};
 
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+
+
 
 const HELP_MESSAGE: &str = "
 Hello there, Human!
@@ -19,15 +22,38 @@ I hope that resolves your issue!
 — HelpBot 🤖
 ";
 
-const HELP_COMMAND: &str = "!help";
+const HELP_COMMAND: &str = "_help";
+const LATINIFY_COMMAND: &str = "_latinify";
 
 struct Handler;
+
+#[derive(Deserialize)]
+struct Latinify {
+    msg: String,
+}
+
+async fn latinify(msg: String) -> Result<String, Box<dyn std::error::Error>> {
+    let request_url = format!("http://localhost:8000/{}/", msg);
+    let resp = reqwest::get(&request_url)
+        .await?;
+    let latinify: Latinify = resp.json().await?;
+    return Ok(latinify.msg)
+}
+
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == HELP_COMMAND {
             if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        else if msg.content == LATINIFY_COMMAND {
+            let latinify_res = latinify(msg.content).await.unwrap();
+
+            
+            if let Err(why) = msg.channel_id.say(&ctx.http, latinify_res).await {
                 println!("Error sending message: {:?}", why);
             }
         }
